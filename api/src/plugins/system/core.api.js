28cafd6d519b54131308621cafac3c35f5dc500e
Lucas <lucas.neme@sisloc.com.br>
2023-09-14 15:24:53 -0300

    Primeiro Commit Confeitaria

---
+++ api/src/plugins/system/core.api.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+function Rest (config) {
+
+}
+
+module.exports = {
+    Rest,
+    async setup () {
+        console.log('Init Api')
+    }
+}
